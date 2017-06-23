4327ae2a462a4945b711ace3129a5904fe001bd8
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-06-23 18:26:33 +0200

    Fixed config paths

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -7,9 +7,9 @@ require_once "../vendor/autoload.php";
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/../sectionfield/src/Entities"), $isDevMode);
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/../src/Entities"), $isDevMode);
 // or if you prefer yaml or XML
-$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/../sectionfield/src/config/xml"), $isDevMode);
+$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/../src/config/xml"), $isDevMode);
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 
 // database configuration parameters
