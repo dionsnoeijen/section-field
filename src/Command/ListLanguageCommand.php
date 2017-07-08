a321be56f57f6125cac87f7fdf415926b4b1c2f1
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-07-08 15:33:56 +0200

    Added list language command and test

--- src/Command/ListLanguageCommand.php
+++ src/Command/ListLanguageCommand.php
@@ -4,8 +4,13 @@ declare (strict_types=1);
 namespace Tardigrades\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Tardigrades\Entity\EntityInterface\Application;
+use Tardigrades\Entity\EntityInterface\Language;
 use Tardigrades\SectionField\SectionFieldInterface\LanguageManager;
 
 class ListLanguageCommand extends Command
@@ -24,11 +29,49 @@ class ListLanguageCommand extends Command
     {
         $this
             ->setDescription('List language')
-            ->setHelp('List language');
+            ->setHelp('List all installed languages');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Language');
+        $languages = $this->languageManager->readAll();
+
+        $this->renderTable($output, $languages);
+    }
+
+    private function renderTable(OutputInterface $output, array $languages): void
+    {
+        $table = new Table($output);
+
+        $rows = [];
+        /** @var Language $language */
+        foreach ($languages as $language) {
+
+            $applications = $language->getApplications();
+            $applicationText = '';
+            /** @var Application $application */
+            foreach ($applications as $application) {
+                $applicationText .= (string) $application->getName() . "\n";
+            }
+
+            $rows[] = [
+                $language->getId(),
+                (string) $language->getI18n(),
+                $applicationText,
+                $language->getCreated()->format('D-m-y'),
+                $language->getUpdated()->format('D-m-y')
+            ];
+        }
+
+        $rows[] = new TableSeparator();
+        $rows[] = [
+            new TableCell('<info>All installed languages</info>', ['colspan' => 6])
+        ];
+
+        $table
+            ->setHeaders(['#id', 'name', 'handle', 'sections', 'languages', 'created', 'updated'])
+            ->setRows($rows)
+        ;
+        $table->render();
     }
 }
---
+++ test/unit/Command/ListLanguageCommandTest.php
@@ -0,0 +1,97 @@
+<?php
+declare (strict_types=1);
+
+namespace Tardigrades\Command;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Tardigrades\Entity\Language;
+use Tardigrades\SectionField\SectionFieldInterface\LanguageManager;
+use Tardigrades\Entity\Application as ApplicationEntity;
+
+final class ListLanguageCommandTest extends TestCase
+{
+    /**
+     * @var LanguageManager
+     */
+    private $languageManager;
+
+    /**
+     * @var ListLanguageCommand
+     */
+    private $listLanguageCommand;
+
+    /**
+     * @var Application
+     */
+    private $application;
+
+    public function setUp()
+    {
+        $this->languageManager = Mockery::mock(LanguageManager::class);
+        $this->listLanguageCommand = new ListLanguageCommand($this->languageManager);
+        $this->application = new Application();
+        $this->application->add($this->listLanguageCommand);
+    }
+
+    private function givenAnArrayOfLanguages(): array
+    {
+        return [
+            (new Language())
+                ->setId(1)
+                ->setI18n('nl_NL')
+                ->addApplication(
+                    (new ApplicationEntity())
+                        ->setId(1)
+                        ->setName('Application name')
+                )
+                ->addApplication(
+                    (new ApplicationEntity())
+                        ->setId(2)
+                        ->setName('Another application name')
+                )
+                ->setUpdated(new \DateTime())
+                ->setCreated(new \DateTime()),
+            (new Language())
+                ->setId(2)
+                ->setI18n('en_EN')
+                ->addApplication(
+                    (new ApplicationEntity())
+                        ->setId(1)
+                        ->setName('Again, a name')
+                )
+                ->addApplication(
+                    (new ApplicationEntity())
+                        ->setId(2)
+                        ->setName('Fffff, name')
+                )
+                ->setUpdated(new \DateTime())
+                ->setCreated(new \DateTime()),
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_list_field_types()
+    {
+        $command = $this->application->find('sf:list-language');
+        $commandTester = new CommandTester($command);
+
+        $languages = $this->givenAnArrayOfLanguages();
+
+        $this->languageManager
+            ->shouldReceive('readAll')
+            ->once()
+            ->andReturn($languages);
+
+        $commandTester->execute(['command' => $command->getName()]);
+
+        $this->assertRegExp(
+            '/All installed languages/',
+            $commandTester->getDisplay()
+        );
+    }
+}
