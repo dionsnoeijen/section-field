c9d34def7ba44fab7b4641b32e30601177f4415e
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-07-08 13:55:09 +0200

    Added delete language command and test

--- src/Command/DeleteLanguageCommand.php
+++ src/Command/DeleteLanguageCommand.php
@@ -4,14 +4,31 @@ declare (strict_types=1);
 namespace Tardigrades\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Tardigrades\SectionField\SectionFieldInterface\ApplicationManager;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
+use Tardigrades\Entity\EntityInterface\Application;
+use Tardigrades\Entity\EntityInterface\Language;
 use Tardigrades\SectionField\SectionFieldInterface\LanguageManager;
+use Tardigrades\SectionField\Service\LanguageNotFoundException;
+use Tardigrades\SectionField\ValueObject\Id;
 
 class DeleteLanguageCommand extends Command
 {
+    /**
+     * @var LanguageManager
+     */
     private $languageManager;
+
+    /**
+     * @var QuestionHelper
+     */
+    private $questionHelper;
 
     public function __construct(
         LanguageManager $languageManager
@@ -25,11 +42,90 @@ class DeleteLanguageCommand extends Command
     {
         $this
             ->setDescription('Delete language')
-            ->setHelp('Delete language');
+            ->setHelp('Shows a list of installed languages, choose the language you would like to delete.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Language');
+        $this->questionHelper = $this->getHelper('question');
+
+        $this->showInstalledLanguages($input, $output);
+    }
+
+    private function showInstalledLanguages(InputInterface $input, OutputInterface $output): void
+    {
+        $languages = $this->languageManager->readAll();
+
+        $this->renderTable($output, $languages);
+        $this->deleteWhatRecord($input, $output);
+    }
+
+    private function getLanguageRecord(InputInterface $input, OutputInterface $output): Language
+    {
+        $question = new Question('<question>What record do you want to delete?</question> (#id): ');
+        $question->setValidator(function ($id) use ($output) {
+            try {
+                return $this->languageManager->read(Id::create((int) $id));
+            } catch (LanguageNotFoundException $exception) {
+                $output->writeln("<error>{$exception->getMessage()}</error>");
+            }
+            return null;
+        });
+
+        return $this->questionHelper->ask($input, $output, $question);
+    }
+
+    private function deleteWhatRecord(InputInterface $input, OutputInterface $output): void
+    {
+        $language = $this->getLanguageRecord($input, $output);
+
+        $output->writeln('<info>Record with id #' . $language->getId() . ' will be deleted</info>');
+
+
+        $sure = new ConfirmationQuestion('<comment>Are you sure?</comment> (y/n) ', false);
+
+        if (!$this->questionHelper->ask($input, $output, $sure)) {
+            $output->writeln('<comment>Cancelled, nothing deleted.</comment>');
+            return;
+        }
+        $this->languageManager->delete($language);
+
+        $output->writeln('<info>Removed!</info>');
+    }
+
+    private function renderTable(OutputInterface $output, array $languages): void
+    {
+        $table = new Table($output);
+
+        $rows = [];
+        /** @var Language $language */
+        foreach ($languages as $language) {
+
+            $applications = $language->getApplications();
+            $applicationText = '';
+            /** @var Application $application */
+            foreach ($applications as $application) {
+                $applicationText .= $application->getName() . "\n";
+            }
+
+            $rows[] = [
+                $language->getId(),
+                (string) $language->getI18n(),
+                $applicationText,
+                $language->getCreated()->format('D-m-y'),
+                $language->getUpdated()->format('D-m-y')
+            ];
+        }
+
+        $rows[] = new TableSeparator();
+        $rows[] = [
+            new TableCell('<info>All installed languages</info>', ['colspan' => 6])
+        ];
+
+        $table
+            ->setHeaders(['#id', 'i18n', 'application', 'created', 'updated'])
+            ->setRows($rows)
+        ;
+        $table->render();
     }
 }
---
+++ test/unit/Command/DeleteLanguageCommandTest.php
@@ -0,0 +1,115 @@
+<?php
+declare (strict_types=1);
+
+namespace Tardigrades\Command;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Tardigrades\Entity\Language;
+use Tardigrades\Entity\Application as ApplicationEntity;
+use Tardigrades\SectionField\Service\LanguageManager;
+
+/**
+ * @coversDefaultClass Tardigrades\Command\DeleteLanguageCommand
+ * @covers ::<private>
+ * @covers ::__construct
+ */
+final class DeleteLanguageCommandTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @var LanguageManager|Mockery\MockInterface
+     */
+    private $languageManager;
+
+    /**
+     * @var DeleteLanguageCommand
+     */
+    private $deleteLanguageCommand;
+
+    /**
+     * @var Application
+     */
+    private $application;
+
+    public function setUp()
+    {
+        $this->languageManager = Mockery::mock(LanguageManager::class);
+        $this->deleteLanguageCommand = new DeleteLanguageCommand($this->languageManager);
+        $this->application = new Application();
+        $this->application->add($this->deleteLanguageCommand);
+    }
+
+    private function givenAnArrayOfLanguages()
+    {
+        return [
+            (new Language())
+                ->setId(1)
+                ->setI18n('nl_NL')
+                ->addApplication(
+                    (new ApplicationEntity())
+                        ->setId(1)
+                        ->setName('Application name')
+                )
+                ->addApplication(
+                    (new ApplicationEntity())
+                        ->setId(2)
+                        ->setName('Another application name')
+                )
+                ->setUpdated(new \DateTime())
+                ->setCreated(new \DateTime()),
+            (new Language())
+                ->setId(2)
+                ->setI18n('en_EN')
+                ->addApplication(
+                    (new ApplicationEntity())
+                        ->setId(1)
+                        ->setName('Again, a name')
+                )
+                ->addApplication(
+                    (new ApplicationEntity())
+                        ->setId(2)
+                        ->setName('Fffff, name')
+                )
+                ->setUpdated(new \DateTime())
+                ->setCreated(new \DateTime()),
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_delete_application_with_id_1()
+    {
+        $command = $this->application->find('sf:delete-language');
+        $commandTester = new CommandTester($command);
+
+        $fields = $this->givenAnArrayOfLanguages();
+
+        $this->languageManager
+            ->shouldReceive('readAll')
+            ->once()
+            ->andReturn($fields);
+
+        $this->languageManager
+            ->shouldReceive('read')
+            ->once()
+            ->andReturn($fields[0]);
+
+        $this->languageManager
+            ->shouldReceive('delete')
+            ->once();
+
+        $commandTester->setInputs([1, 'y']);
+        $commandTester->execute(['command' => $command->getName()]);
+
+        $this->assertRegExp(
+            '/Removed!/',
+            $commandTester->getDisplay()
+        );
+    }
+}
