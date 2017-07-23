cea785017ab93d7bbbc14f24786c50d6a2aa774c
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-07-23 14:13:59 +0200

    Added text area field type

---
+++ src/FieldType/TextArea/TextArea.php
@@ -0,0 +1,11 @@
+<?php
+declare (strict_types=1);
+
+namespace Tardigrades\FieldType\TextArea;
+
+use Tardigrades\FieldType\FieldType;
+use Tardigrades\FieldType\TextArea\TextAreaInterface\TextArea as TextAreaInterface;
+
+class TextArea extends FieldType implements TextAreaInterface
+{
+}
