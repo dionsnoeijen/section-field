cefbed0b0328115acf691a980b0fa742ad4411f1
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-09-08 15:37:20 +0200

    Making the reader more advanced

--- src/SectionField/ValueObject/ReadOptions.php
+++ src/SectionField/ValueObject/ReadOptions.php
@@ -12,9 +12,19 @@ class ReadOptions
     const ID = 'id';
     const SLUG = 'slug';
     const SECTION = 'section';
+    const SECTION_ID = 'sectionId';
     const LIMIT = 'limit';
     const OFFSET = 'offset';
     const ORDER_BY = 'orderBy';
+    const SORT = 'sort';
+    const SORT_ASC = 'ASC';
+    const SORT_DESC = 'DESC';
+    const BEFORE = 'before';
+    const AFTER = 'after';
+    const LOCALE_ENABLED = 'localeEnabled';
+    const LOCALE = 'locale';
+    const SEARCH = 'search';
+    const FIELD = 'field';
 
     /** @var array */
     private $options;
@@ -32,7 +42,7 @@ class ReadOptions
             $valid = true;
         }
 
-        if ($options['section'] instanceof FullyQualifiedClassName) {
+        if ($options[self::SECTION] instanceof FullyQualifiedClassName) {
             $valid = true;
         }
 
@@ -47,7 +57,7 @@ class ReadOptions
     {
         $sectionEntities = [];
 
-        if ($this->options['section'] instanceof FullyQualifiedClassName) {
+        if ($this->options[self::SECTION] instanceof FullyQualifiedClassName) {
             $sectionEntities = [$this->options[self::SECTION]];
         }
 
@@ -64,6 +74,22 @@ class ReadOptions
         return $sectionEntities;
     }
 
+    public function getSectionId(): ?int
+    {
+        try {
+            Assertion::keyIsset($this->options, self::SECTION_ID,
+                'The sectionId is not set'
+            );
+            Assertion::integerish($this->options[self::SECTION_ID],
+                'The sectionId needs to be an integer'
+            );
+        } catch (InvalidArgumentException $exception) {
+            return null;
+        }
+
+        return (int) $this->options[self::SECTION_ID];
+    }
+
     public function getOffset(): ?int
     {
         try {
@@ -77,7 +103,7 @@ class ReadOptions
             return null;
         }
 
-        return (int) $this->options['offset'];
+        return (int) $this->options[self::OFFSET];
     }
 
     public function getLimit(): ?int
@@ -89,10 +115,11 @@ class ReadOptions
             Assertion::integerish($this->options[self::LIMIT],
                 'The limit needs to be an integer.'
             );
-            return (int) $this->options[self::LIMIT];
         } catch (InvalidArgumentException $exception) {
             return null;
         }
+
+        return (int) $this->options[self::LIMIT];
     }
 
     public function getOrderBy(): ?array
@@ -104,10 +131,97 @@ class ReadOptions
             Assertion::isArray($this->options[self::ORDER_BY],
                 'Order by needs to be an array. Example: (["some" => "ASC"])'
             );
-            return (array) $this->options[self::ORDER_BY];
         } catch (InvalidArgumentException $exception) {
             return null;
         }
+
+        return (array) $this->options[self::ORDER_BY];
+    }
+
+    /**
+     * @todo: Probably unnecessary method
+     * @return null|string
+     */
+    public function getSort(): ?string
+    {
+        try {
+            Assertion::choice($this->options[self::SORT], [self::SORT_ASC, self::SORT_DESC], 'Sort option incorrect');
+        } catch (InvalidArgumentException $exception) {
+            return null;
+        }
+
+        return (string) $this->options[self::SORT];
+    }
+
+    public function getBefore(): ?\DateTime
+    {
+        try {
+            Assertion::keyIsset($this->options, self::BEFORE, 'Before is not defined');
+            Assertion::string($this->options[self::BEFORE]);
+        } catch (InvalidArgumentException $exception) {
+            return null;
+        }
+
+        return new \DateTime($this->options[self::BEFORE]);
+    }
+
+    public function getAfter(): ?\DateTime
+    {
+        try {
+            Assertion::keyIsset($this->options, self::AFTER, 'After is not defined');
+            Assertion::string($this->options[self::AFTER]);
+        } catch (InvalidArgumentException $exception) {
+            return null;
+        }
+
+        return new \DateTime($this->options[self::AFTER]);
+    }
+
+    public function getLocaleEnabled(): ?bool
+    {
+        try {
+            Assertion::keyIsset($this->options, self::LOCALE_ENABLED, 'localeEnabled is not set');
+            Assertion::boolean($this->options[self::LOCALE_ENABLED]);
+        } catch (InvalidArgumentException $exception) {
+            return null;
+        }
+
+        return (bool) $this->options[self::LOCALE_ENABLED];
+    }
+
+    public function getLocale(): ?string
+    {
+        try {
+            Assertion::keyIsset($this->options, self::LOCAlE, 'No locale defined');
+            Assertion::string($this->options, 'Locale is supposed to be a string like en_EN');
+        } catch (InvalidArgumentException $exception) {
+            return null;
+        }
+
+        return (string) $this->options[self::LOCALE];
+    }
+
+    public function getSearch(): ?string
+    {
+        try {
+            Assertion::keyIsset($this->options, self::SEARCH, 'No search defined');
+            Assertion::string($this->options, 'The search term must be a string');
+        } catch (InvalidArgumentException $exception) {
+            return null;
+        }
+
+        return (string) $this->options[self::SEARCH];
+    }
+
+    public function field(): ?Handle
+    {
+        try {
+            $handle = Handle::fromString($this->options[self::FIELD]);
+        } catch (InvalidArgumentException $exception) {
+            return null;
+        }
+
+        return $handle;
     }
 
     public function getId(): ?Id
@@ -115,11 +229,11 @@ class ReadOptions
         try {
             Assertion::keyIsset($this->options, self::ID, 'This id is not set');
             Assertion::digit($this->options[self::ID], 'The id is not numeric');
-
-            return Id::fromInt($this->options[self::ID]);
         } catch (InvalidArgumentException $exception) {
             return null;
         }
+
+        return Id::fromInt($this->options[self::ID]);
     }
 
     public function getSlug(): ?Slug
--- src/Twig/SectionTwigExtension.php
+++ src/Twig/SectionTwigExtension.php
@@ -28,6 +28,17 @@ class SectionTwigExtension extends Twig_Extension
         );
     }
 
+    /**
+     * Returns entries that belong to a specific section.
+     * In the future it will be possible to fetch entries of
+     * multiple sections at once.
+     *
+     * A section can be passed by it's FQCN: Fully.Qualified.Example
+     * Or by it's handle: example
+     *
+     * @param string $section
+     * @return SectionTwigExtension
+     */
     public function section(string $section): SectionTwigExtension
     {
         $this->options[ReadOptions::SECTION] = $section;
@@ -35,6 +46,24 @@ class SectionTwigExtension extends Twig_Extension
         return $this;
     }
 
+    /**
+     * Returns entries that belong to a specific section by a section ID.
+     *
+     * @param int $sectionId
+     * @return SectionTwigExtension
+     */
+    public function sectionId(int $sectionId): SectionTwigExtension
+    {
+        // @todo: Implement sectionId
+        return $this;
+    }
+
+    /**
+     * Limit returned entries
+     *
+     * @param int $limit
+     * @return SectionTwigExtension
+     */
     public function limit(int $limit): SectionTwigExtension
     {
         $this->options[ReadOptions::LIMIT] = $limit;
@@ -42,6 +71,12 @@ class SectionTwigExtension extends Twig_Extension
         return $this;
     }
 
+    /**
+     * Offset returned entries
+     *
+     * @param int $offset
+     * @return SectionTwigExtension
+     */
     public function offset(int $offset): SectionTwigExtension
     {
         $this->options[ReadOptions::OFFSET] = $offset;
@@ -49,6 +84,12 @@ class SectionTwigExtension extends Twig_Extension
         return $this;
     }
 
+    /**
+     * Specify which field to order by
+     *
+     * @param array $orderBy
+     * @return SectionTwigExtension
+     */
     public function orderBy(array $orderBy): SectionTwigExtension
     {
         $this->options[ReadOptions::ORDER_BY] = $orderBy;
@@ -56,6 +97,131 @@ class SectionTwigExtension extends Twig_Extension
         return $this;
     }
 
+    /**
+     * Specify sort direction
+     *
+     * ASC|DESC
+     *
+     * @param string $sort
+     * @return SectionTwigExtension
+     */
+    public function sort(string $sort): SectionTwigExtension
+    {
+        // @todo implement sort
+        return $this;
+    }
+
+    /**
+     * Only fetch entries with a Post Date that is before the given date.
+     *
+     * @param string $before
+     * @return SectionTwigExtension
+     */
+    public function before(string $before): SectionTwigExtension
+    {
+        $before = new \DateTime($before);
+
+        // @todo: Implement before
+        return $this;
+    }
+
+    /**
+     * Only fetch entries with a Post Date that is on or after the given date.
+     *
+     * @param string $after
+     * @return SectionTwigExtension
+     */
+    public function after(string $after): SectionTwigExtension
+    {
+        $after = new \DateTime($after);
+
+        // @todo: Implement after
+        return $this;
+    }
+
+    /**
+     * Provides ability to get entries that are for another locale. (v1.1)
+     *
+     * @param bool $enabled
+     * @return SectionTwigExtension
+     */
+    public function localeEnabled(bool $enabled = true): SectionTwigExtension
+    {
+        // @todo: Implement localeEnabled
+        return $this;
+    }
+
+    /**
+     * Fetch just the entries for a specific locale.
+     *
+     * @param string $locale
+     * @return SectionTwigExtension
+     */
+    public function locale(string $locale): SectionTwigExtension
+    {
+        // @todo: Implement locale
+        return $this;
+    }
+
+    /**
+     * Only fetch the entry with the given ID.
+     *
+     * @param int $id
+     * @return SectionTwigExtension
+     */
+    public function id(int $id): SectionTwigExtension
+    {
+        $this->options[ReadOptions::ID] = $id;
+
+        return $this;
+    }
+
+    /**
+     * Only fetch the entry with the given slug.
+     *
+     * @param string $slug
+     * @return SectionTwigExtension
+     */
+    public function slug(string $slug): SectionTwigExtension
+    {
+        $this->options[ReadOptions::SLUG] = $slug;
+
+        return $this;
+    }
+
+    /**
+     * This method requires some extra thinking.
+     * How are we going to handle searching in the context of
+     * SexyField intelligently? What version of SexyField will
+     * this be available?
+     *
+     * @param string $searchQuery
+     * @return SectionTwigExtension
+     */
+    public function search(string $searchQuery): SectionTwigExtension
+    {
+        // @todo Implement search
+        return $this;
+    }
+
+    /**
+     * Return entries based on a field value.
+     * Example, based on a title with a value: "Awesome Title"
+     *
+     * @param string $fieldHandle
+     * @param string $value
+     * @return SectionTwigExtension
+     */
+    public function field(string $fieldHandle, string $value): SectionTwigExtension
+    {
+        // @todo: Implement query by specific field and it's value.
+        return $this;
+    }
+
+    /**
+     * @param array $options
+     * @return \ArrayIterator
+     */
     public function fetch(array $options = []): \ArrayIterator
     {
         $options = array_merge($this->options, $options);
