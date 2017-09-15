6680cc4083ffe29894f8f7a3c256e0ad9fee62ab
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-09-15 13:42:02 +0200

    Removed redundant uses

--- src/Command/GenerateSectionCommand.php
+++ src/Command/GenerateSectionCommand.php
@@ -3,9 +3,6 @@ declare (strict_types=1);
 
 namespace Tardigrades\Command;
 
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Helper\TableCell;
-use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
