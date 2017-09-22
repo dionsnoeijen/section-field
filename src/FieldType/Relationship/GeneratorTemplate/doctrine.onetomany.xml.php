752e990303f69b84a628d56f1e4704cc94e66260
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-09-22 14:10:12 +0200

    Generated new example xml config

--- src/FieldType/Relationship/GeneratorTemplate/doctrine.onetomany.xml.php
+++ src/FieldType/Relationship/GeneratorTemplate/doctrine.onetomany.xml.php
@@ -1,1 +1,1 @@
-<one-to-many field="<?php echo $toPluralHandle; ?>" fetch="EXTRA_LAZY" target-entity="<?php echo $toFullyQualifiedClassName; ?>" mapped-by="<?php echo $fromHandle; ?>" />
+<one-to-many field="<?php echo $toPluralHandle; ?>" target-entity="<?php echo $toFullyQualifiedClassName; ?>" mapped-by="<?php echo $fromHandle; ?>" />
--- src/FieldType/Relationship/GeneratorTemplate/doctrine.onetoone.xml.php
+++ src/FieldType/Relationship/GeneratorTemplate/doctrine.onetoone.xml.php
@@ -1,14 +1,14 @@
 <?php if ($type === 'unidirectional') { ?>
-<many-to-one field="<?php echo $toHandle; ?>" fetch="EXTRA_LAZY" target-entity="<?php echo $toFullyQualfiedClassName; ?>">
+<many-to-one field="<?php echo $toHandle; ?>" target-entity="<?php echo $toFullyQualfiedClassName; ?>">
     <join-column name="<?php echo $toHandle; ?>_id" referenced-column-name="id" />
 </many-to-one>
 <?php } ?>
 
 <?php if ($type === 'bidirectional') { ?>
-<one-to-one field="<?php echo $toHandle; ?>" fetch="EXTRA_LAZY" target-entity="<?php echo $toFullyQualfiedClassName; ?>" mapped-by="<?php echo $fromHandle; ?>" />
+<one-to-one field="<?php echo $toHandle; ?>" target-entity="<?php echo $toFullyQualfiedClassName; ?>" mapped-by="<?php echo $fromHandle; ?>" />
 </entity>
 <entity name="<?php echo $toFullyQualfiedClassName; ?>">
-<one-to-one field="<?php echo $fromHandle; ?>" fetch="EXTRA_LAZY" target-entity="<?php echo $fromFullyQualfiedClassName; ?>" inversed-by="<?php echo $toHandle; ?>">
+<one-to-one field="<?php echo $fromHandle; ?>" target-entity="<?php echo $fromFullyQualfiedClassName; ?>" inversed-by="<?php echo $toHandle; ?>">
     <join-column name="<?php echo $toHandle; ?>_id" referenced-column-name="id" />
 </one-to-one>
 <?php } ?>
